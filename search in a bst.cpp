8d6db1d7a5046ac97c9d0c26cc93ce33012bc4e3
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-25 02:40:18 +0530

    Create search in a bst.cpp

---
+++ search in a bst.cpp
@@ -0,0 +1,13 @@
+TC- O(H)
+SC= O(H)
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(!root)return NULL;
+        if(root->val==val)return root;
+        if(root->val >val)return searchBST(root->left,val);
+        return searchBST(root->right,val);
+        
+    }
+};

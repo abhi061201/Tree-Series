fe7ecaeaf848df49a8cd3e22b64149fd7dfb71c4
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2022-06-29 01:01:22 +0530

    Update inorder  succrssor and predecessor of bst.cpp

--- BST/inorder  succrssor and predecessor of bst.cpp
+++ BST/inorder  succrssor and predecessor of bst.cpp
@@ -24,3 +24,47 @@ void findPreSuc(Node* root, Node*& Pre, Node*& Suc, int key)
     gosuc(root, key,Pre,Suc);
 
 }
+
+TC- O(N)
+SC=O(H)
+
+void findPreSuc(Node* root, Node*& pred, Node*& suc, int key)
+{
+ 
+pred= new Node(INT_MIN);
+suc= new Node (INT_MAX);
+if(!root){
+    pred->key= -1;
+suc->key= -1;
+return;
+
+}
+queue<Node * >q;
+q.push(root);
+while(!q.empty()){
+    
+    Node * cur= q.front();
+    q.pop();
+    
+    if(cur->key <key && pred->key <cur->key )pred= cur;
+    if(cur->key >key && suc->key >cur->key )suc= cur;
+    
+    if(cur->left)q.push(cur->left);
+    if(cur->right)q.push(cur->right);
+    
+    
+}
+
+if(suc->key==INT_MAX)suc->key=-1;
+if(pred->key==INT_MIN)pred->key=-1;
+
+
+
+
+
+
+
+
+// Your code goes here
+
+}
